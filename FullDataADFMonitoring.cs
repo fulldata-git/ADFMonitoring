e46646ce9a7cacb7f2a4881eaeabad2873f104fa
fulldata-git <79724599+fulldata-git@users.noreply.github.com>
2021-02-27 03:22:34 +0530

    Add files via upload

---
+++ FullDataADFMonitoring.cs
@@ -0,0 +1,118 @@
+﻿/*
+███████████████████████████ FULLDATA.NL ██████████████████████████████
+
+███████╗██╗   ██╗██╗     ██╗         ██████╗  █████╗ ████████╗ █████╗ 
+██╔════╝██║   ██║██║     ██║         ██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗
+█████╗  ██║   ██║██║     ██║         ██║  ██║███████║   ██║   ███████║
+██╔══╝  ██║   ██║██║     ██║         ██║  ██║██╔══██║   ██║   ██╔══██║
+██║     ╚██████╔╝███████╗███████╗    ██████╔╝██║  ██║   ██║   ██║  ██║
+╚═╝      ╚═════╝ ╚══════╝╚══════╝    ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝
+███████████████████████████ FULLDATA.NL ██████████████████████████████
+*/
+
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Management.DataFactory;
+using Microsoft.Azure.Management.DataFactory.Models;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Rest;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data;
+
+namespace MonitoringGit
+{
+    public static class FullDataADFMonitoring
+    {
+        [FunctionName("FullDataADFMonitoringFuntion")]
+        public static async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            // This section needs to be updated before running the Project
+            #region Get Config
+            var clientId = "Client Id for the Service Principal registered";
+            var clientSecret = "Client Secret for the Service Principal registered";
+            var tenantId = "Tenant Id for the azure tenant";
+            var subscriptionId = "Subscription Id for the azure subscription";
+            var resourceGroupName = "resourceGroupName for the factory to be queried";
+            var factoryName = "Name of the factory to be queried";
+            #endregion
+
+            //Generate authentication token with the client Id and secret of the registered service Principal
+            var context = new AuthenticationContext("https://login.windows.net/" + tenantId);
+            ClientCredential cc = new ClientCredential(clientId, clientSecret);
+            AuthenticationResult authResult = context.AcquireTokenAsync(
+                "https://management.azure.com/", cc).Result;
+            ServiceClientCredentials cred = new TokenCredentials(authResult.AccessToken);
+
+            // Create Data Factory Client and use the auth token
+            var client = new Microsoft.Azure.Management.DataFactory.DataFactoryManagementClient(cred)
+            {
+                SubscriptionId = subscriptionId
+            };
+            
+            List<RunQueryFilter> listQuery = new List<RunQueryFilter>();
+
+            #region FilterClause and Run Parameters
+            RunQueryFilter queryFilter = new RunQueryFilter()
+            {
+                Operand = "RunStart",
+                OperatorProperty = "Equals",
+                Values = new List<string> { "InProgress" }
+            };
+            listQuery.Add(queryFilter);
+            
+            RunFilterParameters runFilterParameters = new RunFilterParameters()
+            {
+                LastUpdatedAfter = DateTime.UtcNow.AddHours(-10),
+                LastUpdatedBefore = DateTime.UtcNow,
+                Filters = listQuery
+            };
+            #endregion
+
+            //Query Data factory
+            var queryResult = await client.PipelineRuns.QueryByFactoryAsync(resourceGroupName, factoryName, runFilterParameters);
+
+
+            //Serialize the incoming data in a model and store in DB if required
+            List<FullDataADFMonitoringModel> modelList = new List<FullDataADFMonitoringModel>();
+            var serializedResult = JsonConvert.SerializeObject(queryResult?.Value);
+
+            modelList = JsonConvert.DeserializeObject<List<FullDataADFMonitoringModel>>(serializedResult);
+            foreach (var item in modelList)
+            {
+                item.FactoryName = factoryName;
+            }
+            using (System.Data.SqlClient.SqlConnection connectionAzureDB = new SqlConnection("conn string"))
+            {
+                var serilaizedModel = JsonConvert.SerializeObject(modelList);
+                var values = new { AdfPipeLineMonitoringData = "data model to be inserted" };
+                var sqlResults = await connectionAzureDB.QueryAsync("[Internal].[usp_AzureADFPipeLinesExecutionData]", values, commandTimeout: 0, commandType: CommandType.StoredProcedure);
+                connectionAzureDB.Close();
+            }
+
+            // Send Mail using sendgrid to concerned team if a pipeline exceeds configured threshold
+            foreach (var item in modelList)
+            {
+                if (item.Status.ToLower() == "inprogress")
+                {
+                    var timeDiff = DateTime.Now - Convert.ToDateTime(item.RunStart);
+
+                    // Can be configured based on each pipeline in the factory
+                    var threshold = 10;
+
+                    if(threshold<timeDiff.Hours)
+                    {
+                            var sendMailUtility = new FullDataSendMailUtility();
+                            await sendMailUtility.SendMailAsync(item, timeDiff, "sendGridApiKey", threshold);
+                    }
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# ADFMonitoring
-ADF Monitoring Solution by Full Data
+# FullData ADF Monitoring utility
+
+This Azure Data Factory monitoring utility is solution that records and stores pipeline execution runtimes in an Azure SQL Database and sends out an Email alert when a threshold limit is exceeded. Its developed by FullData and provided ‘AS-IS’ to the Microsoft Data Platform community.
+When your Azure Data Factory environment is consuming a lot of resources due some unforeseen failing or /long running job s you would want to be informed right away rather then after receiving a serious bill. An ADF environment easily spans 100+ pipelines that execute b. But not all actions may be properly logged as status ‘Completed’ or and get stuck as ‘InProgress’. The current standard Azure generic monitoring signal logic does not cover the full range of scenarios.
+To avoid surprises and to provide more granular insights and alerting, the FullData team developed a simple and efficient service that will email you when the duration of pipeline activities exceeds 2x the average duration or any other configurable alarm threshold that you as user can set by querying by querying the Azure management API directly. This is more reliable than leveraging the Azure Log Analytics data as source.
+
+
+todo:  
+1) Insert pictures of overall architecture ( adf-> FD monitor -> SQLDB -> sendgrid)  
+2) Inserrt picture of a API call example:
+
+
+Figure 1:  Raw API output.
+
+
+The ADF Monitoring utility will update table [internal].[AzureADFPipeLinesExecutionData] every hour with the latest information: 
+ 
+Figure 2: Sample of the persisted ADF pipeline details
+
+
+The typical scenario to keep an eye on cost, in addition to setting is by levering Azure cost alerts. HoweverHowever, many Azure customers may not have direct insights to their azure cost metrics due to their subscription type, therefor Azure cost alerts might not be an adequate alternative to monitor ADF consumption. In those cases this monitoring approach to check runtime durations direct could be especially of interest to you! 
+
+
+## Installation
+
+Step 1 : Clone the repository
+
+
+Step 2 : Open ADFMonitor.sln
+
+
+Step 3 : Add connection Strings to ADFMonitor.cs
+
+
+Step 4 : Add your ADF specific info and run the program
+
+
+
+```bash
+            #region Get Config
+            var clientId = "Client Id for the Service Principal registered";
+            var clientSecret = "Client Secret for the Service Principal registered";
+            var tenantId = "Tenant Id for the azure tenant";
+            var subscriptionId = "Subscription Id for the azure subscription";
+            var resourceGroupName = "resourceGroupName for the factory to be queried";
+            var factoryName = "Name of the factory to be queried";
+            #endregion
+```
+## Usage
+
+By tracking your ADF pipeline runtime durations, you also have a perfect start to perform trend analysis to predict when your ADF environment runs in overtime.
+
+Easily and visualy identify which pipelines take most time to complete, spotting the outliers which would benefit from optimization and predict when you will run out of your data processing window. 
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## Full Data ADF Monitoring - The complete solution
+
+In addition to the gathering the base ADF monitoring data we can help you visualize trends and resolve ADF pipeline challenges and optimize their runtimes minimizing cost and maximizing efficiency.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
